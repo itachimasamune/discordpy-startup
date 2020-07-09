fb3f9d4f836093d0c844c292653ac9e623b6df64
itachimasamune <68067037+itachimasamune@users.noreply.github.com>
2020-07-09 23:35:44 +0900

    update4
    ランダムにゃーん

--- discordbot.py
+++ discordbot.py
@@ -16,10 +16,12 @@ async def on_ready():
 async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
+        if message.content == 'じゃあの':
+            await message.channel.send('ばいびー')
         return
     # 「/neko」と発言したら「にゃーん」が返る処理
     if message.content == '/neko':
-        await message.channel.send('にゃーん')
+        await message.channel.send(random.choice(('にゃーん','にゅーん')))
     
 @bot.event
 async def on_command_error(ctx, error):
